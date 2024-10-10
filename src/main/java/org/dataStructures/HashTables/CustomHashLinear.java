217b3075f607a5d5c06cb479d78ed12ec621dea4
sazzadtomal <sazzadtomalfordesktop1660@gmail.com>
2024-10-10 16:20:06 +0600

    added separate main file for Hash tables

--- src/main/java/org/dataStructures/HashTables/CustomHashLinear.java
+++ src/main/java/org/dataStructures/HashTables/CustomHashLinear.java
@@ -1,2 +1,93 @@
-package org.dataStructures.HashTables;public class CustomHashLinear {
+package org.dataStructures.HashTables;
+
+import java.util.Arrays;
+
+public class CustomHashLinear {
+
+    Object[][] hashtable=new Object[5][2];
+    int count=0;
+
+    private int hash(int key){
+        return  key%hashtable.length;
+    }
+
+
+
+    public  void print2DArray() {
+        for (int i = 0; i < hashtable.length; i++) { // Loop through rows
+            for (int j = 0; j < hashtable[i].length; j++) { // Loop through columns
+                System.out.print(hashtable[i][j] + " ");
+            }
+            System.out.println(); // Newline after each row
+        }
+    }
+
+
+
+    public void put(int key, String value){
+
+        int hash=hash(key);
+
+        for(int i=hash;i<hashtable.length;i=(i%hashtable.length)+1) {
+            if (hashtable[i][0] != null && (int) hashtable[i][0] == key) {
+                hashtable[i][1] = value;
+                return;
+            }
+        }
+
+        if(count==hashtable.length) throw new IllegalArgumentException("hashmap is full");
+
+        for(int i=hash;i<hashtable.length;i=(i%hashtable.length)+1){
+            if(hashtable[i][0]==null){
+                hashtable[i][0]=key;
+                hashtable[i][1]=value;
+                break;
+            }
+
+        }
+        count++;
+    }
+
+
+    public String get(int key) {
+
+        int hash = hash(key);
+        for (int i = hash; i < hashtable.length; i = (i % hashtable.length) + 1) {
+            if (hashtable[i][0] != null && (int) hashtable[i][0] == key) {
+                return (String)hashtable[i][1];
+            }
+        }
+        throw new IllegalArgumentException("key not present");
+    }
+
+    public void remove(int key) {
+
+        int hash = hash(key);
+        for (int i = hash; i < hashtable.length; i = (i % hashtable.length) + 1) {
+            if (hashtable[i][0] != null && (int) hashtable[i][0] == key) {
+                hashtable[i][0]=null;
+                hashtable[i][1]=null;
+                count--;
+                return;
+            }
+        }
+        throw new IllegalArgumentException("key not present");
+    }
+
+
+
+
+
+
+
+
+    public int size(){
+        return count;
+    }
+
+
+
+
+
+
 }
--- src/main/java/org/dataStructures/HashTables/Main.java
+++ src/main/java/org/dataStructures/HashTables/Main.java
@@ -1,2 +1,35 @@
-package org.dataStructures.HashTables;public class Main {
+package org.dataStructures.HashTables;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        int[] array={1,4,3,4,5,6,9,11,11,11};
+//        System.out.println(MostFrequentOccurrence.mostFrequentInt(array));
+
+//        int[] array={1, 7, 9,9,9};
+//        System.out.println(MostFrequentOccurrence.pairDifferenceIsK(array,2));
+
+//         int[] array={3,3,2,4};
+//         MostFrequentOccurrence.twoSum(array,6);
+
+        CustomHashLinear hashmap=new CustomHashLinear();
+
+        hashmap.put(11,"ball");
+        hashmap.put(12,"bat");
+        hashmap.put(13,"tennis");
+        hashmap.put(14,"table");
+        hashmap.put(1,"foot");
+
+
+
+        hashmap.print2DArray();
+
+
+
+
+
+    }
 }
